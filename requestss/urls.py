250d83ec35795fd9cbe26cf6dd31457dc1126880
Nekit269 <nkkab2@mail.ru>
2020-05-28 10:20:14 +0500

    Добавил обработку сохранения и загрузки настроек

--- requestss/urls.py
+++ requestss/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     re_path(r'^create_account$', views.registration),
     re_path(r'^save_history$', views.save_history),
     re_path(r'^load_history$', views.load_history),
+    re_path(r'^save_settings$', views.save_settings),
+    re_path(r'^load_settings$', views.load_settings),
 ]
